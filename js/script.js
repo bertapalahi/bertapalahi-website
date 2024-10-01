7b0977ac3547d578915cadb60d72cb5c277be0f5
ber <93647413+bertapalahi@users.noreply.github.com>
2024-10-01 11:04:20 +0000

    new gallery

--- js/script.js
+++ js/script.js
@@ -12,18 +12,6 @@ function closeLightbox() {
     lightbox.style.display = 'none';
 }
 */
-// galeria masonry
-$('.grid').masonry({
-  columnWidth: '.grid-sizer',
-  itemSelector: '.grid-item',
-  gutter: 10, // Espacio entre las imágenes
-  fitWidth: true // Ajustar el ancho de la cuadrícula al contenedor
-});
-
-var msnry = new Masonry( '.grid', {
-    columnWidth: 200,
-    itemSelector: '.grid-item'
-  });
 
 //eliminar botó dret
 document.addEventListener('contextmenu', function(e) {
